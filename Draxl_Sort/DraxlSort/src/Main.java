5e1a655647fbaa5a599fee82cf4a9342f84f9c1a
luggi-lintner <lulintner@tsn.at>
2018-12-11 10:15:43 +0100

    LinkedList V1

--- Draxl_Sort/DraxlSort/src/Main.java
+++ Draxl_Sort/DraxlSort/src/Main.java
@@ -1,23 +1,91 @@
 import java.util.*;
 public class Main {
+	
+	
 
+	public static ArrayList<Integer> getRandomList(int size)
+	{
+		ArrayList<Integer> zahlen = new ArrayList<Integer>();
+		Random r = new Random();
+		for(int i=0; i<size; i++)
+		{
+			zahlen.add(r.nextInt(1000));
+		}
+		
+		return zahlen;
+		 
+	}
+	
+	public static ArrayList<Integer> Quick(ArrayList<Integer> zahlen)
+	{
+		if(zahlen.size()<=1)
+		{	
+			return(zahlen);
+			
+		}
+		
+		int pivot=0;
+		int zufallszahl=0;
+		ArrayList<Integer> lower = new ArrayList<Integer>();
+		ArrayList<Integer> higher = new ArrayList<Integer>();
+		
+		ArrayList<Integer> erg = new ArrayList<Integer>();
+		Random zufall = new Random(); 
+		zufallszahl=zufall.nextInt(zahlen.size());
+		pivot=zahlen.get(zufallszahl);
+		System.out.println(pivot);
+		
+		for(int i=1; i<zahlen.size(); i++)
+		{
+			if(i==pivot)
+				continue;
+			if(zahlen.get(i)<pivot)
+			{
+				lower.add(zahlen.get(i));
+			}
+			else
+			{
+				higher.add(zahlen.get(i));
+			} 
+		}
+		
+		erg.addAll(Quick(lower));
+		erg.add(pivot);
+		erg.addAll(Quick(higher));
+		
+		return erg;
+		
+		}
+		
+		
+		
+		
+	
+	
+	
 	public static void main(String[] args) 
 	{
 		
-		ArrayList<Integer> zahlen = new ArrayList<Integer>();
+		ArrayList<Integer> zahlen = new ArrayList();
+		
+		
+		
+		
+			
+		
+		
+		
 		
 		zahlen.add(8);
-		zahlen.add(7);
 		zahlen.add(4);
-		zahlen.add(10);
-		zahlen.add(3);
-		zahlen.add(17);
-		zahlen.add(11);
-		zahlen.add(-4);
+		zahlen.add(7);
+		zahlen.add(5);
+		zahlen.add(9);
 		
 		int x=0;
+		long zeit;
 		
-		
+		long startzeit= System.currentTimeMillis();
 		for(int j=1; j<zahlen.size(); j++)
 		{
 			for(int i=0; i<zahlen.size(); i++ )
@@ -31,10 +99,23 @@ public class Main {
 				}
 				
 			}
+				
 			
-			
-			System.out.println(zahlen);
 		}
+		
+		/*System.out.println(zahlen);
+		System.out.println("\n");
+		long endzeit = System.currentTimeMillis();
+		zeit = endzeit - startzeit;
+		
+		
+		
+		System.out.println("gemessene Zeit: "+ zeit + " Millisekunden");*/
+		
+		System.out.println(Quick(zahlen));
+	
+		
 	}
+	
 
 }
