4e6d08e7a978aefd4633053d579ddb67225e7d9d
luggi-lintner <lulintner@tsn.at>
2019-01-22 08:05:31 +0100

    fertig

---
+++ Binary_Tree/BST/src/BST/Node.java
@@ -0,0 +1,24 @@
+package BST;
+
+public class Node {
+	
+	int value;
+	Node left;
+	Node right;
+	
+	
+	Node (int value)
+	{
+		this.value=value;
+		right=null;
+		left=null;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
